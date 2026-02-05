4d4229f88728628c692c7c11a1ceb11595a58ecb
kaanorkanpi <78913378+kaanorkanpi@users.noreply.github.com>
2026-02-05 16:16:45 +0100

    continued beastgame

--- day03/kmain.py
+++ day03/kmain.py
@@ -6,8 +6,21 @@ def clear_screen():
 
 def wait_for_space():
         while True:
-            if input("\nPress SPACE then Enter to continue... ") == " ":
+            if input("\n\n\n(Press SPACE then Enter to continue...)") == " ":
                 return
+'''
+    ,o0MMMMMMMMNMMMMM8888888888888888MMMMMM.88
+  8888888888V'.o      OOO      OOO     o. V8
+  8888LLLLl:      O     O ___ O       O    D88,
+   8888888LLb        \____OOOOOO____/       '8888
+    8888888888booooooOlllllllllllOoooooood8888
+      
+    ,o0MMMMMMMMNMMMMM8888888888888888MMMMMM.88
+  8888888888V'.o    OOO        OOO     o. V8
+  8888LLLLl:     X     O  !!  O       X   D88,
+   8888888LLb    \___OOOOOOOOOOOO___/    '8888
+    8888888888booooooOlllllllllllOoooooood8888'''
+
 
 print('''
       
@@ -30,17 +43,91 @@ print('''
 ''')
 
 wait_for_space()
-time.sleep(2)
+time.sleep(1)
 clear_screen()
 
-print("After a long period of work you decided to take some time off and went on a cruise to travel the world.")
+print("\n\n\nAfter a long period of work you decided to take some time off and went on a cruiseship to travel the world.")
+print("\n\nThe cruise will go from the Netherlands over the atlantic ocean and is headed towards Guyana in Southamerica.")
 wait_for_space()
 clear_screen()
 
-print("The cruise will go from the Netherlands over the atlantic ocean and is headed towards Guyana in Southamerica.")
+print("\n\n\nWhile you're in the cabins to get some sleep you feel heavy rumbling and loud noises in the middle of the night and get out of your bed...")
+print("\n\n...you hear screaming and open your door, people are panicking and the ship is tilting...")
 wait_for_space()
 clear_screen()
 
-print("While you're in the cabins .")
+print("\n\n\n...You run down the hall and try to get outside, just as you reach the corner you hear a sharp hiss from a pipe when it suddenly bursts...")
+print("\n\n...for a split-second you have a tinnitus and then everything turned white, you fainted...")
 wait_for_space()
 clear_screen()
+
+print("\n\n\nWhen you finally wake up, you cough up loads of water and appear to be at a beach, next to debris from the cruisheship you were on.")
+
+print("""\n\n
+                    #####
+                   #### = =
+                   ##C    >
+                    _)' _( .' ,
+                 __/ |_/\   " *. o
+                /` \_\ \/     %`= '_  .
+               /  )   \/|      .^',*. ,
+              /' /-   o/       - " % '_
+             /\_/     <       = , ^ ~ .
+             )_o|----'|          .`  '
+         ___// (_  -   (
+        ///-(  #\_ \   \ 
+    """
+)
+wait_for_space()
+clear_screen()
+
+print("\n\n\nIt looks like there is noone else than you...")
+print("\n\nYou walk down the beach trying to find someone else or any signs of society...")
+wait_for_space()
+clear_screen()
+
+
+print("\n\n\nAfter what feels like hours of walking you suddenly spot MASSIVE footprints in the wet sand...")
+print("""
+\n\n
+                          .----.
+                 ______.  |    | 
+   _ _ _      _o'       \ `----'
+  o     `-- -'           o .---. 
+  o                      o `---' 
+  o                      o .--.
+  o                      o `--'
+  o      __ _            o .--.
+   - - -'    ` -o_      /  `--'
+                  `----' .--.
+                         `--''
+                         
+        """
+            )
+wait_for_space()
+clear_screen()
+
+
+print("\n\n\nWhat do you want to do?")
+choice1 = input("\n\n(Type Follow/Avoid/Cover Tracks) ").lower()
+
+if choice1 == "follow":
+    clear_screen()
+    print("\n\n\nYou follow the tracks and find a torn satchel.\n")
+    wait_for_space()
+    clear_screen()
+    choice2 = input("\n\n\nDo you want to open it?(y/n)\n")
+    if choice2 == "y":
+        clear_screen()
+        print("\n\n\nThe torn satchel contains:\n\nHalf-map\nStrange Tooth\nRusty Knife\n")
+        wait_for_space()
+        clear_screen()
+
+     
+# if choice1 == "Avoid":
+
+# if choice1 == "Cover Tracks":
+     
+
+# wait_for_space()
+# clear_screen()
